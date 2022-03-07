748ae2cfae8d5d650846f2cd0e3c8772474e6b2e
jeremy-grondin <j.grondin@student.isartdigital.com>
2022-03-07 16:18:43 +0100

    Rework health bar to use event Action to update the UI

--- Assets/Scripts/GenericCharacters/GenericCharacterStats.cs
+++ Assets/Scripts/GenericCharacters/GenericCharacterStats.cs
@@ -7,22 +7,12 @@ public class GenericCharacterStats : MonoBehaviour
     protected int life;
     [SerializeField] protected int maxMana;
     protected int mana;
-    
-    public HealthBar healthBar;
 
-    AnimatorHandler anim;
+    protected AnimatorHandler anim;
 
-    private void Start()
-    {
-        life = maxLife;
-        healthBar.SetMaxHealth(maxLife);
-        anim = GetComponentInChildren<AnimatorHandler>();
-    }
-
-    public void TakeDamage(int damage)
+    public virtual void TakeDamage(int damage)
     {
         life -= damage;
-        healthBar.SetCurrentHealth(life);
         
         if(life <= 0)
             anim.PlayAnimation("Death", true);
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,6 +1,19 @@
+using System;
 using UnityEngine;
 
 public class PlayerStats : GenericCharacterStats
 {
+    public static Action<int> OnLifeChange;
+
+    void Start()
+    {
+        life = maxLife;
+        anim = GetComponentInChildren<AnimatorHandler>();
+    }
     
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        OnLifeChange?.Invoke(life);
+    }
 }
